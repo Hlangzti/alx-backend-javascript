0a8ba49aed8c38e380529a1b6cda3b1312b3f951
Hlangzti <hlangzt@gmail.com>
2023-06-27 11:16:51 +0000

    task0-10

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const data = values[0];
+      const user = values[1];
+      console.log(`${data.body} ${user.firstName} ${user.lastName}`);
+    }, () => {
+      console.log('Signup system offline');
+    });
+}
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((d) => d.map((r) => ({
+    status: r.status,
+    value: r.value ? r.value : r.reason.message,
+  })));
+}
